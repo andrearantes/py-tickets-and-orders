83a9e7bc9c9a52683a9d31119032ca5e27d93b31
Andre Marcio <andre.marcio.ti@gmail.com>
2026-07-24 14:42:01 -0300

    verify task

--- db/models.py
+++ db/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
-    def __str__(self) -> str:
+    def str(self) -> str:
         return self.name
 
 
@@ -12,17 +14,17 @@ class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
 
-    def __str__(self) -> str:
+    def str(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
 
-    def __str__(self) -> str:
+    def str(self) -> str:
         return self.title
 
 
@@ -35,18 +37,71 @@ class CinemaHall(models.Model):
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
 
-    def __str__(self) -> str:
+    def str(self) -> str:
         return self.name
 
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(
-        to=CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions"
-    )
-    movie = models.ForeignKey(
-        to=Movie, on_delete=models.CASCADE, related_name="movie_sessions"
-    )
+    cinema_hall = models.ForeignKey(to=CinemaHall, on_delete=models.CASCADE,
+                                    related_name="movie_sessions")
+    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE,
+                              related_name="movie_sessions")
 
-    def __str__(self) -> str:
+    def str(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to="User",
+                             on_delete=models.CASCADE,
+                             related_name="orders")
+
+    def __str__(self):
+        return f"<Order: {self.created_at}>"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(to=MovieSession,
+                                      on_delete=models.CASCADE,
+                                      related_name="tickets")
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE,
+                              related_name="tickets")
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self):
+        return (f"<Ticket: {self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
+                f"(row: {self.row}, "
+                f"seat: {self.seat})>")
+
+    def clean(self):
+        super().clean()
+        if (self.movie_session_id and self.movie_session and
+                self.movie_session.cinema_hall):
+            hall = self.movie_session.cinema_hall
+            if self.row > hall.rows:
+                raise ValidationError({"row": f"row number must be in "
+                                              f"available range: (1, rows): "
+                                              f"(1, {hall.rows})"})
+            if self.seat > hall.seats_in_row:
+                raise ValidationError({"seat": f"seat number must be in"
+                                               f" available range: (1, seats_in_row): "
+                                               f"(1, {hall.seats_in_row})"})
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=["movie_session", "row", "seat"],
+                                    name="unique_ticket")]
+
+
+class User(AbstractUser):
+    pass
