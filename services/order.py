5f9c51d867b586face870d5854e49c783c689d5f
Andre Marcio <andre.marcio.ti@gmail.com>
2026-07-24 19:18:40 -0300

    verify task

---
+++ services/order.py
@@ -0,0 +1,21 @@
+from db.models import Ticket, User, Order, MovieSession
+from django.db import transaction
+
+
+@transaction.atomic
+def create_order(username, tickets, date=None, **kwargs) -> User:
+    user = User.objects.get(username=username)
+
+    order = Order(user=user)
+    order.save()
+    if date:
+        Order.objects.filter(pk=order.pk).update(created_at=date)
+    for ticket in tickets:
+        movie_session = MovieSession.objects.get(id=ticket["movie_session"])
+        Ticket.objects.create(
+            order=order,
+            movie_session=movie_session,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
+    return order
